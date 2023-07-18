f0c9bc2936f17b879f3139ae42c0890b7b54de55
junjunghasudungan <junjunghasudungan43@gmail.com>
2023-07-18 08:04:32 +0700

    fix in register getToken

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -27,35 +27,32 @@
     <div class="border-lg border-gray-900 mt-2 p-2 px-8 py-2"> 
         <div class="px-4 py-4 justify-center items-center  bg-red-300  rounded-lg border border-gray-500 p-2 ">
             <div class="w-full sm:max-w-md mt-auto px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                <div id="title" class="w-full p-8 px-py mb-2 px-2 py-2 justify-items-center">
+                    <h1 class="w-full font-bold text-white item-center text-center">Form Login</h1>
+                </div>
                 <form   @submit.prevent="handleLogin()"
                         class="justify-center sm:justify-center items-center">
                     <div class="mb-6">
-                        <label  for="email" 
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                Email address
-                        </label>
                         <input  type="email" 
                                 id="email" 
                                 v-model="form.email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                placeholder="Email"
                                 required>
                     </div> 
                     <div class="mb-6">
-                        <label  for="password" 
-                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                Password
-                        </label>
                         <input  type="password" 
                                 id="password" 
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 v-model="form.password"
+                                placeholder="Password"
                                 required>
                     </div> 
-                    <button type="submit" 
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Submit
-                    </button>
-                    <router-link to="/register"> You dont have account? Register</router-link>
+                        <button type="submit" 
+                                class="text-white justify-items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Submit
+                        </button>
+                        <router-link to="/register"> You dont have account? Register</router-link>
                 </form>
             </div>
         </div>
--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,7 +3,7 @@
     import { ref } from "vue";
     import { useRouter } from 'vue-router';
 
-    const router = useRouter();
+    const route = useRouter();
 
     const form = ref({
         name: '',
@@ -12,7 +12,15 @@
         password_confirmation: ''
     })
 
+    // create some function to handle cookies 
+    const getToken = async () => {
+        await axios.get('/sanctum/csrf-cookie');
+    }
+
     const handleRegister = async () => {
+        //  call getToken 
+        await getToken();
+        
         await axios.post('/register', {
             name: form.value.name,
             email: form.value.email,
@@ -20,19 +28,19 @@
             password_confirmation: form.value.password_confirmation,
         });
 
-        router.push("/");
+        route.push("/");
     }
-
+// password
 </script>
 
 <template>
     <div class="border-lg border-gray-900 mt-2 p-2 px-8 py-2"> 
         <div class="px-4 py-4 justify-center items-center  bg-red-300  rounded-lg border border-gray-500 p-2 ">
             <div class="w-full sm:max-w-md mt-auto px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                <div id="title" class="w-full p-8 px-py mb-2 px-2 py-2 justify-items-center">
+                <div id="title" class="w-full px-py mb-2 px-2 py-2 justify-items-center">
                     <h1 class="w-full font-bold text-white item-center text-center">Form Register</h1>
                 </div>
-                <form   @submit.prevent="handleRegister"
+                <form   @submit.prevent="handleRegister()"
                         class="justify-center sm:justify-center items-center">
                     <div class="mb-6">
                         <input  type="text" 
@@ -63,7 +71,7 @@
                     <div class="mb-6">
                         <input type="password" 
                                 id="confirm_password" 
-                                v-model="form.pasword_confirmation"
+                                v-model="form.password_confirmation"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                                 placeholder="Password Confirmation" 
                                 required>
