880fd25f990f612af45ebf6e8b7ee1e48b1a3cda
junjunghasudungan <junjunghasudungan43@gmail.com>
2023-07-21 12:52:31 +0700

    preparation to fetching data API

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -14,7 +14,7 @@
 
 <template>
     <div class="border-lg border-gray-900 mt-2 p-2 px-8 py-2"> 
-        <div class="px-4 py-4 justify-center items-center  bg-red-300  rounded-lg border border-gray-500 p-2 ">
+        <div class="px-4 py-4 justify-center items-center  bg-gray-200  rounded-lg border border-gray-500 p-2 ">
             <div class="w-full sm:max-w-md mt-auto px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 <div id="title" class="w-full px-py mb-2 px-2 py-2 justify-items-center">
                     <h1 class="w-full font-bold text-white item-center text-center">Form Register</h1>
@@ -68,7 +68,7 @@
                             <span class="text-red-400 text-xs m-1 p-1"> {{ authStore.errors.password[0] }}</span>
                        </div>
                     </div> 
-                    <div class="mb-6">
+                    <div class="mb-4">
                         <input
                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           id="confirm_password"
@@ -83,10 +83,15 @@
                             <span class="text-red-400 font-xs m-1 p-1">  {{ authStore.errors.password[0] }} </span>
                         </div>
                     </div> 
-                    <button type="submit" 
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                            Submit
-                    </button>
+                    <div class="flex-auto justify-self-end space-x-2"> 
+                      <button type="submit" 
+                              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                              Submit
+                      </button>
+                      <router-link :to="{ name: 'Login' }">
+                        <span class="m-2 p-2 px-2 py-2 text-xs flex-auto text-bold text-gray-400 hover:text-gray-200"> you already have account?</span>
+                      </router-link>
+                    </div>
                 </form>
             </div>
         </div>
--- src/views/PostIndex.vue
+++ src/views/PostIndex.vue
@@ -1,6 +1,7 @@
 <script setup>
     import { ref, onMounted } from 'vue';
     import usePost from '../composables/post';
+    import PostCard from '../components/PostCard.vue';
     // create some variable to collect all data pasts
 
     const { posts, post, getPosts,  errors } = usePost();
@@ -11,7 +12,13 @@
 </script>
 
 <template>
-    <div>
-        <h1 class="text-center">Halaman Post Index</h1>
+    <div class="w-full bg-gray-200">
+        <div class="mt-4 px-2 py-2 bg-yellow-200 ">
+            <h1 class="text-center">Halaman Post Index</h1>
+        </div>
+        <div class="w-full px-4 py-4">
+            <PostCard />
+        </div>
+
     </div>
 </template>
